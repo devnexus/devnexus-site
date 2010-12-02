166c74310be69e874ef27d10b629c35519780810
ghillert@gmail.com <ghillert@gmail.com@1fa30ceb-3983-0cc1-c120-9294f86c98d8>
2010-12-02 05:15:29 +0000

    git-svn-id: https://devnexus.googlecode.com/svn/trunk@26 1fa30ceb-3983-0cc1-c120-9294f86c98d8

---
+++ ting/ting-web/src/main/java/com/devnexus/ting/core/dao/hibernate/SystemDaoHibernate.java
@@ -0,0 +1,90 @@
+package com.devnexus.ting.core.dao.hibernate;
+
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.hibernate.HibernateException;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.ejb.Ejb3Configuration;
+import org.hibernate.tool.hbm2ddl.SchemaExport;
+import org.hibernate.tool.hbm2ddl.SchemaUpdate;
+import com.devnexus.ting.core.dao.SystemDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.stereotype.Repository;
+
+@Repository("systemDao")
+public class SystemDaoHibernate implements SystemDao {
+
+    private @Autowired DataSource dataSource;
+    private @Autowired LocalContainerEntityManagerFactoryBean fb;
+
+    /* (non-Javadoc)
+     * @see org.jrecruiter.service.DemoService#createDatabase()
+     */
+    @Override
+    public void updateDatabase() {
+
+        final Ejb3Configuration cfg = new Ejb3Configuration();
+        final Ejb3Configuration configured = cfg.configure( fb.getPersistenceUnitInfo(), fb.getJpaPropertyMap() );
+        final Configuration configuration = configured.getHibernateConfiguration();
+
+        HibernateHack.dataSource = dataSource;
+
+        Properties props = new Properties();
+        props.put("hibernate.connection.provider_class",
+         "org.jrecruiter.service.impl.DemoServiceImpl.HibernateHack");
+
+
+        final org.hibernate.tool.hbm2ddl.SchemaUpdate schemaUpdate;
+
+        try {
+            schemaUpdate = new SchemaUpdate(configuration, props);
+        } catch (HibernateException e) {
+            throw new IllegalStateException(e);
+        }
+
+        schemaUpdate.execute(true, true);
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.jrecruiter.service.DemoService#createDatabase()
+     */
+    @Override
+    public void createDatabase(boolean outputOnly, String dialect) {
+
+        final Ejb3Configuration cfg = new Ejb3Configuration();
+
+        Map properties = fb.getJpaPropertyMap();
+
+        if (dialect != null) {
+        	properties.put("hibernate.dialect", dialect);
+        }
+
+        final Ejb3Configuration configured = cfg.configure( fb.getPersistenceUnitInfo(), fb.getJpaPropertyMap() );
+        final Configuration configuration = configured.getHibernateConfiguration();
+
+        final SchemaExport schemaExport;
+
+        try {
+            schemaExport = new SchemaExport(configuration, dataSource.getConnection());
+        } catch (HibernateException e) {
+            throw new IllegalStateException(e);
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+
+        schemaExport.create(true, !outputOnly);
+
+    }
+
+    private static class HibernateHack {
+        public static DataSource dataSource;
+    }
+
+
+}
--- ting/ting-web/src/main/java/com/devnexus/ting/core/dao/jpa/PresentationDaoJpa.java
+++ ting/ting-web/src/main/java/com/devnexus/ting/core/dao/jpa/PresentationDaoJpa.java
@@ -1,5 +1,7 @@
 package com.devnexus.ting.core.dao.jpa;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.devnexus.ting.core.dao.PresentationDao;
@@ -13,5 +15,27 @@ public class PresentationDaoJpa extends GenericDaoJpa< Presentation, Long>
     public PresentationDaoJpa() {
         super(Presentation.class);
     }
-	
+
+	@Override
+	public List<Presentation> getPresentationsForCurrentEvent() {
+		return super.entityManager
+			.createQuery("select p from Presentation p "
+					   + "left join p.event e "
+					   + "where e.current = :iscurrent "
+					   + "order by p.title ASC", Presentation.class)
+			.setParameter("iscurrent", true)
+			.getResultList();
+	}
+
+	@Override
+	public List<Presentation> getPresentationsForEvent(Long eventId) {
+		return super.entityManager
+		.createQuery("select p from Presentation p "
+				   + "    join p.event e "
+				   + "where e.id = :eventId "
+				   + "order by p.title ASC", Presentation.class)
+		.setParameter("eventId", eventId)
+		.getResultList();
+	}
+
 }
