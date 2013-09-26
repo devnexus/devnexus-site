2e8df099911077bea433dcba93a28e48227ab72b
Summers Pittman <secondsun@gmail.com>
2013-09-25 20:33:49 -0400

    Adding in updates to users table

--- src/main/resources/data/migration.sql
+++ src/main/resources/data/migration.sql
@@ -2,3 +2,7 @@ alter table events add created_date timestamp without time zone;
 alter table events add updated_date timestamp without time zone;
 alter table events add version integer;
  
+alter table users add created_date timestamp without time zone;
+alter table users add updated_date timestamp without time zone;
+alter table users add version integer;
+
