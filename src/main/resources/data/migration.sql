abbc368ca401e8909e7d85d5af179626e44ced75
Summers Pittman <secondsun@gmail.com>
2013-10-19 11:56:05 -0400

    Adding in SQL updates

--- src/main/resources/data/migration.sql
+++ src/main/resources/data/migration.sql
@@ -6,3 +6,46 @@ alter table users add created_date timestamp without time zone;
 alter table users add updated_date timestamp without time zone;
 alter table users add version integer;
 
+CREATE TABLE cfp_submissions
+(
+  id bigint NOT NULL,
+  created_date timestamp without time zone,
+  updated_date timestamp without time zone,
+  version integer,
+  bio character varying(255),
+  first_name character varying(255),
+  google_plus_id character varying(255),
+  last_name character varying(255),
+  linked_in_id character varying(255),
+  twitter_id character varying(255),
+  description character varying(255),
+  email character varying(255),
+  phone character varying(255),
+  picture_file2 bytea,
+  presentation_type bigint,
+  session_recording_approved boolean NOT NULL,
+  skill_level bigint,
+  slot_preference character varying(255),
+  title character varying(255),
+  topic character varying(255),
+  tshirt_size character varying(255),
+  picture bigint,
+  event bigint,
+  CONSTRAINT cfp_submissions_pkey PRIMARY KEY (id),
+  CONSTRAINT fk_93jnud4hv6d4pykxfur3luak1 FOREIGN KEY (event)
+      REFERENCES events (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT fk_srgk1qvqa9tx8hrnfa703bifc FOREIGN KEY (picture)
+      REFERENCES file_data (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE cfp_submissions
+  OWNER TO devnexus;
+
+ALTER TABLE organizers ADD COLUMN linked_in_id character varying(255);
+
+ALTER TABLE speakers ADD COLUMN linked_in_id character varying(255);
+
