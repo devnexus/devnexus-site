db34d2d6c5252358b87c7e735e14bac94da2f01e
Gunnar Hillert <gunnar@hillert.com>
2013-10-26 21:42:42 -0400

    Make the email CC configurable

--- src/main/java/com/devnexus/ting/core/service/impl/CfpToMailTransformer.java
+++ src/main/java/com/devnexus/ting/core/service/impl/CfpToMailTransformer.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2002-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.devnexus.ting.core.service.impl;
 
 import javax.mail.MessagingException;
@@ -5,18 +20,35 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.util.Assert;
 import org.stringtemplate.v4.ST;
 
 import com.devnexus.ting.common.SystemInformationUtils;
 import com.devnexus.ting.core.model.CfpSubmission;
 
+/**
+ * Converts a {@link CfpSubmission} and converts it to a {@link MimeMessage}.
+ *
+ * @author Gunnar Hillert
+ *
+ */
 public class CfpToMailTransformer {
 
 	private JavaMailSender mailSender;
 	private String fromUser;
+	private String ccUser;
 
 	public void setFromUser(String fromUser) {
 		this.fromUser = fromUser;
+	}
+
+	public void setCcUser(String ccUser) {
+		Assert.hasText(ccUser, "The CC Email Address must not be empty or null.");
+		this.ccUser = ccUser;
+	}
+
+	public void setMailSender(JavaMailSender mailSender) {
+		this.mailSender = mailSender;
 	}
 
 	public MimeMessage prepareMailToSpeaker(CfpSubmission cfpSubmission) {
@@ -35,11 +67,15 @@ public class CfpToMailTransformer {
 
 			messageHelper.setFrom(fromUser);
 			messageHelper.setTo(cfpSubmission.getEmail());
-			messageHelper.setCc("test@hillert.com");
+
+			if (this.ccUser != null) {
+				messageHelper.setCc(this.ccUser);
+			}
+
 			messageHelper.setSubject("DevNexus 2014 - CFP - " + cfpSubmission.getFirstLastName());
 
 		} catch (MessagingException e) {
-			throw new IllegalStateException("Error creating mail message.", e);
+			throw new IllegalStateException("Error creating mail message for CFP: " + cfpSubmission, e);
 		}
 
 		return messageHelper.getMimeMessage();
@@ -68,9 +104,4 @@ public class CfpToMailTransformer {
 
 		return renderedTemplate;
 	}
-
-	public void setMailSender(JavaMailSender mailSender) {
-		this.mailSender = mailSender;
-	}
-
 }
