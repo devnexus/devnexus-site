efdbde123bee4d626c89bb6fea1582103089ad6b
Summers Pittman <secondsun@gmail.com>
2016-08-31 04:24:48 -0400

    More FK fixes

--- src/main/java/com/devnexus/ting/model/CouponCode.java
+++ src/main/java/com/devnexus/ting/model/CouponCode.java
@@ -20,6 +20,7 @@ import java.math.BigDecimal;
 import java.util.Objects;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
@@ -46,6 +47,7 @@ public class CouponCode extends BaseModelObject {
     @ManyToOne
     @XmlTransient
     @NotNull
+    @JoinColumn(name = "TICKET_GROUP")
     @JsonIgnore
     private TicketGroup ticketGroup;
 
