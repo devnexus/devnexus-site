5316a643d0c14f1ab23308ad1ccfa22b78c2a768
lauramoore <laura.moore@rightbox.com>
2017-11-14 09:06:16 -0500

    command filters json data

--- _plugins/jekyll-cfp/lib/jekyll/commands/cfp.rb
+++ _plugins/jekyll-cfp/lib/jekyll/commands/cfp.rb
@@ -5,7 +5,10 @@ module Jekyll
         def init_with_program(prog)
           prog.command(:cfp) do |c|
             c.action do |args, options|
-              Jekyll.logger.info "TODO CFP!"
+              track_data_file = File.read("_data/promo_events.json")
+              event_data = JSON.parse(track_data_file)
+              track_data = event_data['events'].select{|item|"workshop" == item['track']}
+              Jekyll.logger.info track_data.size
             end
           end
         end
