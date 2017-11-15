12b182d448a63f7dcb14b00feaa951cd320f9033
lauramoore <laura.moore@rightbox.com>
2017-11-14 19:23:00 -0500

    working jekyll command to convert promo_events.json into presentation pages

--- _plugins/jekyll-cfp/lib/jekyll/commands/cfp.rb
+++ _plugins/jekyll-cfp/lib/jekyll/commands/cfp.rb
@@ -5,10 +5,15 @@ module Jekyll
         def init_with_program(prog)
           prog.command(:cfp) do |c|
             c.action do |args, options|
-              track_data_file = File.read("_data/promo_events.json")
-              event_data = JSON.parse(track_data_file)
-              track_data = event_data['events'].select{|item|"workshop" == item['track']}
-              Jekyll.logger.info track_data.size
+              event_data_file = File.read("_cfp/promo_events.json")
+              event_data = JSON.parse(event_data_file)
+              workshop_data = event_data['events'].select{|item|"workshop" == item['track']}
+              for event in workshop_data do
+                  event['layout'] = 'preso_details'
+                 filtered_persons = event['persons'].map{ |p| p.select{|k,v| ["full_public_name", "abstract"].include?(k) }}
+                 event['persons'] = filtered_persons
+                 File.write( "_events/#{event['id']}.md", YAML.dump(event)+"\r\n---\r\n" )
+              end
             end
           end
         end
