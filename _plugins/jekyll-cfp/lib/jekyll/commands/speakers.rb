22fddc36fa260294d1c6696761e3e7aed245b7f9
lauramoore <laura.moore@rightbox.com>
2018-01-20 22:08:15 -0500

    build tested speaker handler

--- _plugins/jekyll-cfp/lib/jekyll/commands/speakers.rb
+++ _plugins/jekyll-cfp/lib/jekyll/commands/speakers.rb
@@ -1,10 +1,37 @@
 class Speakers
+  attr_accessor :data
   def initialize(img_host="https://cfp.devnexus.com")
       @img_host = img_host
+      @data = {}
   end
   def full_path_avatar(person)
       img_path = person['avatar_path']
       person['avatar_path'] = "#{@img_host}#{img_path}"
       return person
+  end
+  def count()
+    return @data.size()
+  end
+  # speakers are nested under each event as people,
+  # create a new collection of people, and associate each events id to the
+  # people in that event.  This does not de-dup people.
+  def people_from_events(event_arr)
+     all_people = event_arr.collect do |e|
+         e['people'].collect do |sp| sp.merge({ "events" => [ e["id"]] }) end
+     end
+     return all_people.flatten
+  end
+  # take an array of people and de-dup, keeping all events
+  def organize_people(people_arr, person_data = {})
+      return people_arr.each_with_object(person_data) do | person, personDB |
+        pid = person.delete("id")
+        p_data = personDB.fetch(pid, person)  #defaults to this person
+        p_data["events"] = p_data["events"] | person["events"]
+        personDB[pid] = p_data
+      end
+  end
+  def collect_people_events(event_arr)
+     event_people = people_from_events(event_arr)
+     organize_people(event_people, @data)
   end
 end
--- _plugins/jekyll-cfp/test/test_speaker.rb
+++ _plugins/jekyll-cfp/test/test_speaker.rb
@@ -2,10 +2,79 @@ require 'minitest/autorun'
 require 'jekyll/commands/speakers'
 
 class SpeakersTest < Minitest::Test
-  def test_hello
+  def test_has_default_avatar_path
      speakers = Speakers.new()
      person = {}
      speakers.full_path_avatar(person)
      assert_equal "https://cfp.devnexus.com", person['avatar_path']
+  end
+  def test_collects_1_event_1_speaker
+     speakers = Speakers.new()
+     event_arr = [{ "id" => 1,
+                    "people" => [
+                       { "id" => 11, "abstract" => "foo", "img" => "/foo", "full_public_name" => "Foo A"}
+                    ]
+                  }]
+     results = speakers.people_from_events(event_arr)
+     assert_equal 1, results.size()
+     assert_equal 11, results[0]["id"]
+  end
+  def test_1_event_2_speakers
+     speakers = Speakers.new()
+     event_arr = [{ "id" => 1,
+                    "people" => [
+                       { "id" => 11, "abstract" => "foo", "img" => "/foo", "full_public_name" => "Foo A"},
+                       { "id" => 12, "abstract" => "bar", "img" => "/bar", "full_public_name" => "Bar B"}
+                    ]
+                  }]
+     results = speakers.people_from_events(event_arr)
+     assert_equal 2, results.size()
+     assert_equal 12, results[1]["id"]
+  end
+  def test_2_events_2_speakers_1_common
+     speakers = Speakers.new()
+     event_arr = [{ "id" => 1,
+                    "people" => [
+                       { "id" => 11, "abstract" => "foo", "img" => "/foo", "full_public_name" => "Foo A"},
+                       { "id" => 12, "abstract" => "bar", "img" => "/bar", "full_public_name" => "Bar B"}
+                    ]
+                  },
+                  {"id" => 2,
+                      "people" => [
+                          { "id" => 14, "abstract" => "Boo", "img" => "/boo", "full_public_name" => "D Boo"},
+                          { "id" => 13, "abstract" => "baz", "img" => "/baz", "full_public_name" => "C Baz"}
+                        ]
+                    }]
+     results = speakers.people_from_events(event_arr)
+     assert_equal 4, results.size()
+  end
+  def test_2_people_to_1
+    speakers = Speakers.new()
+    people = [
+      { "id" => 11, "abstract" => "foo", "img" => "/foo", "full_public_name" => "Foo A", "events" => [1]},
+      { "id" => 11, "abstract" => "foo", "img" => "/foo", "full_public_name" => "Foo A", "events" => [2]}
+    ]
+    results = speakers.organize_people(people)
+    assert_equal 1, results.size()
+    p_eleven = results[11]
+    assert_equal([1, 2], p_eleven["events"])
+  end
+  def test_combine_events_to_persons
+    speakers = Speakers.new()
+    event_arr = [{ "id" => 1,
+                   "people" => [
+                      { "id" => 11, "abstract" => "foo", "img" => "/foo", "full_public_name" => "Foo A"}
+                   ]
+                 },
+                 {"id" => 2,
+                     "people" => [
+                         { "id" => 11, "abstract" => "foo", "img" => "/foo", "full_public_name" => "Foo A"},
+                       ]
+                  }]
+    speakers.collect_people_events(event_arr)
+    results = speakers.data()
+    assert_equal 1, results.size()
+    p_eleven = results[11]
+    assert_equal([1, 2], p_eleven["events"])
   end
 end
