dd3647db81e324dd0d6ab3926e4e435bb2ac32e5
lauramoore <laura.moore@rightbox.com>
2018-01-21 15:46:51 -0500

    jekyll-cfp-0.1.0.gem

--- _plugins/jekyll-cfp/lib/jekyll/commands/cfp.rb
+++ _plugins/jekyll-cfp/lib/jekyll/commands/cfp.rb
@@ -13,6 +13,7 @@ module Jekyll
               else
                 Jekyll.logger.info("I do not understand #{args}")
               end
+              #persist_speakers()
             end
           end
         end
@@ -23,9 +24,17 @@ module Jekyll
             days = _data_in.map{ |d| {}.merge( 'index'=> d['index'], 'events' => collect_rooms(d['rooms'])) }
             Jekyll.logger.info("Gathered #{days.length} schedule days")
             _schedule_yaml = YAML.dump(days)
-
-            @speakers.data().each do |k, v| Jekyll.logger.info(v) end
             File.write("_data/schedule.yml", _schedule_yaml)
+            #now we can print all the people
+        end
+        def process_event_data()
+          event_data_file = File.read("_cfp/promo_events.json")
+          event_data = JSON.parse(event_data_file)
+          #workshop_data = event_data['events'].select{|item|"workshop" == item['track']}
+          events = event_data['events']
+          Jekyll.logger.info("Reading #{events.length} events from cfp")
+          @speakers.collect_people_events(events)
+          process_event_collection(events);
         end
         def collect_rooms(room_event_data)
           #Jekyll.logger.info(room_event_data)
@@ -37,62 +46,13 @@ module Jekyll
            times =  array_of_events.map{ |e| e.select{ |k,v| ["id", "start"].include?(k)}.merge("room" => room) }
            #Jekyll.logger.info(times)
            return times
+           process_event_collection(array_of_events)
         end
-        def process_event_data()
-          event_data_file = File.read("_cfp/promo_events.json")
-          event_data = JSON.parse(event_data_file)
-          #workshop_data = event_data['events'].select{|item|"workshop" == item['track']}
-          events = event_data['events']
-          Jekyll.logger.info("Reading #{events.length} events from cfp")
-          stored_speaker_file = File.read("_data/speakers.yml")
-          stored_speaker_data = YAML.load(stored_speaker_file)
-          #Jekyll.logger.info(stored_speaker_data)
-          updated_speaker_data = process_event_collection(stored_speaker_data, events);
-          updated_speaker_yaml = YAML.dump(updated_speaker_data)
-          #Jekyll.logger.info(updated_speaker_yaml)
-          File.write("_data/speakers.yml", updated_speaker_yaml)
-        end
-        def speakers(people, person_details)
-          captured = people | person_details
-          new_people = person_details - people
-        end
-        def correct_speaker_data(speaker_db, person_details)
-          speaker_record = speaker_db.fetch(person_details['id'], nil)
-          if(speaker_record)
-            #TODO - update any cfp sourced images?
-            #Jekyll.logger.info("found")
-          else
-            new_record = { "avatar_path" => person_details['avatar_path'], "name" => person_details['full_public_name']}
-            speaker_db.store(person_details['id'], new_record)
-            #Jekyll.logger.info("new>>> #{speaker_db.fetch(person_details['id'])}")
-          end
-        end
-        def process_event_collection(speaker_db, cfp_data)
-          people = {};
+        def process_event_collection(cfp_data)
           for event in cfp_data do
               person_details = event.delete 'persons'
-              #correct relative avatar paths
-              full_path_persons = person_details.map{|p_item| full_path_avatar(p_item)}
-              process_event(event, full_path_persons)
-              event_link = event.select{ |k,v| ["id", "title"].include?(k)}
-              for person in full_path_persons do
-                person_record = people.fetch(person['id'], person)
-                person_events = person_record.fetch('events', [])
-                person_record['events'] = person_events
-                person_events.push(event_link)
-                people.store(person['id'], person_record)
-              end
+              process_event(event, person_details)
           end
-          Jekyll.logger.info("Collected #{people.values.length} presenters with events")
-          for person in people.values do
-            abstract = person.delete 'abstract'
-            public_items = person.select{ |k,v| ["full_public_name", "id", "twitter_name", "events"].include?(k)}
-            public_items['title'] = person['full_public_name']
-            public_items['layout'] = "speaker_bio"
-            write_item("speakers", public_items, abstract)
-            correct_speaker_data(speaker_db, person)
-          end
-          return speaker_db
         end
         def process_event(event, persons)
          #Jekyll.logger.info(event)
@@ -118,10 +78,37 @@ module Jekyll
           File.write( filename , YAML.dump(item)+"\r\n---\r\n"+abstract)
           #end
         end
-        def full_path_avatar(person)
-          img_path = person['avatar_path']
-          person['avatar_path'] = "https://cfp.devnexus.com#{img_path}"
-          return person
+
+        def persist_speakers()
+          stored_speaker_file = File.read("_data/speakers.yml")
+          stored_speaker_data = YAML.load(stored_speaker_file)
+          #Jekyll.logger.info(stored_speaker_data)
+          for person in @speakers.data().values do
+            abstract = person.delete 'abstract'
+            public_items = person.select{ |k,v| ["full_public_name", "id", "twitter_name", "events"].include?(k)}
+            public_items['title'] = person['full_public_name']
+            public_items['layout'] = "speaker_bio"
+            write_item("speakers", public_items, abstract)
+            correct_speaker_data(stored_speaker_data, person)
+          end
+          updated_speaker_yaml = YAML.dump(stored_speaker_data)
+          #Jekyll.logger.info(updated_speaker_yaml)
+          File.write("_data/speakers.yml", updated_speaker_yaml)
+        end
+        def correct_speaker_data(speaker_db, person_details)
+          speaker_record = speaker_db.fetch(person_details['id'], nil)
+          if(speaker_record)
+            #TODO - update any cfp sourced images?
+            #Jekyll.logger.info("found")
+          else
+            new_record = { "avatar_path" => person_details['avatar_path'], "name" => person_details['full_public_name']}
+            speaker_db.store(person_details['id'], new_record)
+            #Jekyll.logger.info("new>>> #{speaker_db.fetch(person_details['id'])}")
+          end
+        end
+        def speakers(people, person_details)
+          captured = people | person_details
+          new_people = person_details - people
         end
       end
     end
