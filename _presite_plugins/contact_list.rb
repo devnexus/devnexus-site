3da2688f7380e781287b555cc69b6d8042f992a2
Laura Moore <laura.moore@rightbox.com>
2020-01-09 03:34:01 +0000

    produce workshop contact list

--- _presite_plugins/contact_list.rb
+++ _presite_plugins/contact_list.rb
@@ -1,12 +1,14 @@
 require 'json'
-event_data_file = File.read("promo_events.json")
+require 'net/http'
+dataurl = URI('https://cfp.devnexus.com/en/dn2020/public/promo_events.json')
+event_data_file = Net::HTTP.get(dataurl)
 event_data = JSON.parse(event_data_file)
-events = event_data['events'].select{|item|"Workshop" == item['track']}
+events = event_data['events'].select{|item|"Full day Workshops (Wednesday Only)" == item['track']}
 #events = event_data['events']
 puts "Reading #{events.length} events from cfp"
 for event in events do
-  puts "#{event["id"]},#{event["title"]}"
-  for person in event["persons"] do
-    puts "#{person["id"]}, #{person["full_public_name"]}, #{person["email"]}"
-  end 
+  puts "#{event["id"]},#{event["title"]}, #{event["persons"].map{ |p| p["email"]}.join(";")}"
+  #for person in event["persons"] do
+    #puts "#{person["id"]}, #{person["full_public_name"]}, #{person["email"]}"
+  #end 
 end
