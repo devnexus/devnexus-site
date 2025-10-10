9c4688b65dfb05b263fa5e1acc4136b148468432
Laura Moore <laura.moore@rightbox.com>
2025-10-10 12:19:31 -0400

    preping for listings

--- _presite_plugins/jekyll-cfp/lib/jekyll/commands/cfp.rb
+++ _presite_plugins/jekyll-cfp/lib/jekyll/commands/cfp.rb
@@ -14,7 +14,7 @@ module Jekyll
             end
           end
           pcmd.command(:events) do |e|
-            url = "https://sessionize.com/api/v2/csk3n1qx/view/Sessions"
+            url = "https://sessionize.com/api/v2/juzvwep9/view/Sessions"
             e.description "process promo_events.json"
             e.action do |args, options|
                edata = (options['file']) ? read_data(options['file']) : fetch_data(url)
